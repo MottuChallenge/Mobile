bc6b7716179cd368791f08bfa6715c020554fc1d
ThiagoThmaz <kasuma17satto@gmail.com>
2025-05-20 10:33:18 -0300

    Refactor: Mudando Style dos integrantes

--- app/integrantes.tsx
+++ app/integrantes.tsx
@@ -16,7 +16,7 @@ const integrantes = [
   },
   {
     nome: 'Thiago Thomaz Sales Conceição',
-    rm: '557992 ',
+    rm: '557992',
     github: "https://github.com/ThiagoThmaz",
     imagem: 'https://avatars.githubusercontent.com/u/142420181?v=4',
   }
@@ -25,7 +25,6 @@ const integrantes = [
 export default function Integrantes() {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.titulo}>Nosso Time</Text>
 
       {integrantes.map((integrante, index) => (
         <View key={index} style={styles.card}>
@@ -33,10 +32,6 @@ export default function Integrantes() {
           <Text style={styles.nome}>{integrante.nome}</Text>
 
           <View style={styles.links}>
-           
-          
-
-
             <TouchableOpacity
               style={styles.botao}
               onPress={() => Linking.openURL(integrante.github)}
@@ -54,23 +49,23 @@ const styles = StyleSheet.create({
   container: {
     paddingVertical: 50,
     paddingHorizontal: 20,
-    backgroundColor: '#e6ffe6',
+    backgroundColor: '#1a1a1a', // fundo escuro para um contraste
     alignItems: 'center',
   },
   titulo: {
     fontSize: 28,
     fontWeight: 'bold',
     marginBottom: 30,
-    color: '#006400',
+    color: '#66ff66', // verde claro para o título
   },
   card: {
-    backgroundColor: '#ccffcc',
+    backgroundColor: '#333333', // fundo preto suave para os cartões
     width: '100%',
     borderRadius: 16,
     padding: 20,
     marginBottom: 20,
     alignItems: 'center',
-    elevation: 3,
+    elevation: 5,
   },
   imagem: {
     width: 100,
@@ -78,20 +73,20 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     marginBottom: 15,
     borderWidth: 2,
-    borderColor: '#006400',
+    borderColor: '#66ff66', 
   },
   nome: {
     fontSize: 20,
     fontWeight: '600',
     marginBottom: 15,
-    color: '#013220',
+    color: '#66ff66', 
   },
   links: {
     flexDirection: 'row',
     gap: 10,
   },
   botao: {
-    backgroundColor: '#00cc66',
+    backgroundColor: '#006400', 
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 25,
