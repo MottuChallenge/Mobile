098ca1708b640231b7883d5eea7cf9eaddf51915
Pedro Henrique <ph6704938@gmail.com>
2025-11-03 23:11:56 -0300

    Refactor: Update findSetores method to correctly filter sectors by yardId and enhance data structure

--- src/api/setores.ts
+++ src/api/setores.ts
@@ -6,16 +6,40 @@ const URL_API = `${BASE_URL}sectors`;
 
 export class SetoresApi implements ISetoresApi {
   async findSetores(yardId: string): Promise<Sector[]> {
-    const fullUrl = `${URL_API}/${yardId}`;
+    const fullUrl = `${URL_API}`;
     console.log("🔎 Buscando setores:", fullUrl);
 
     try {
       const res = await axios.get(fullUrl);
-      return res.data;
+      const data: Sector[] = res.data;
+      for(let i = 0; i < data.length; i++) {
+
+        data[i].pointsOfInterest = res.data[i].points;
+      }
+
+      const sectorsByYard = data.filter((sector) => sector.yardId === yardId);
+      
+      return sectorsByYard;
     } catch (error: any) {
       console.error("❌ Erro ao buscar setores:", error.message);
       console.error("Detalhes:", error.response?.data || error);
       return [];
+    }
+  }
+  async findSetorById(sectorId: string): Promise<Sector | null> {
+    const fullUrl = `${URL_API}/${sectorId}`;
+    console.log("🔎 Buscando setor por ID:", fullUrl);
+
+    try {
+      const res = await axios.get(fullUrl);
+      const data: Sector = res.data;
+      data.pointsOfInterest = res.data.points;
+      console.log("Setor encontrado:", data);
+      return data;
+    } catch (error: any) {
+      console.error("❌ Erro ao buscar setor:", error.message);
+      console.error("Detalhes:", error.response?.data || error);
+      return null;
     }
   }
 }
